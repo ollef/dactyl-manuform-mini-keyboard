bd9a231ff73da3c96ca65224bbff82e5697683e6
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-10-22 22:15:01 +0200

    Move screws inside case

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -650,13 +650,13 @@
          (translate (map + offset [(first position) (second position) (/ height 2)])))))
 
 (defn screw-insert-all-shapes [bottom-radius top-radius height]
-  (union (screw-insert 0 0         bottom-radius top-radius height [11 10 0])
-         (screw-insert 0 lastrow   bottom-radius top-radius height [0 0 0])
+  (union (screw-insert 0 0         bottom-radius top-radius height [5.5 5.25 0])
+         (screw-insert 0 lastrow   bottom-radius top-radius height [-1 3 0])
         ;  (screw-insert lastcol lastrow  bottom-radius top-radius height [-5 13 0])
         ;  (screw-insert lastcol 0         bottom-radius top-radius height [-3 6 0])
-         (screw-insert lastcol lastrow  bottom-radius top-radius height [0 12 0])
-         (screw-insert lastcol 0         bottom-radius top-radius height [0 7 0])
-         (screw-insert 1 lastrow         bottom-radius top-radius height [0 -16 0])))
+         (screw-insert lastcol lastrow  bottom-radius top-radius height [-1 13 0])
+         (screw-insert lastcol 0         bottom-radius top-radius height [-1 5.5 0])
+         (screw-insert 1 lastrow         bottom-radius top-radius height [-0.5 -13 0])))
 
 ; Hole Depth Y: 4.4
 (def screw-insert-height 4)
